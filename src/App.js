73ff4ab56441f93e556de0091790d8e8b4711db4
Michael Fernandez <112970281+Michael-J-Fernandez@users.noreply.github.com>
2023-03-05 11:51:39 -0500

    Fetch data from API and displayed character names on browser.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
+const URL = "https://rickandmortyapi.com/api/character/?page=1";
+
 function App() {
+
+  const [ charData, setCharData ] = useState({})
+  
+  useEffect(()=>{
+    console.log("effect ran!")
+    fetch(URL)
+    .then(res => res.json())
+    .then(data => setCharData(data))
+  }, [])
+  
+  
+  console.log(typeof charData.results)
+  console.log("This is my Char Data: ", charData.results)
+  
+  
+  // const charList = charData.results.map(char => <h3>{char["name"]}</h3>)
+
   return (
-    <div className="App">
-      <h1>Hi, this is my Rick and Morty App!</h1>
+    <div>
+      {/* {charList} */}
+      {charData.results.map((char) => (
+        <h3>{char.name}</h3>
+      ))}
     </div>
   );
 }
