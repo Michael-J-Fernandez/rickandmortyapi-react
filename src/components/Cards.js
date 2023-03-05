06b3797987046c8da0c5192c65e2858f52ac35d7
Michael Fernandez <112970281+Michael-J-Fernandez@users.noreply.github.com>
2023-03-05 18:37:08 -0500

    Updated Card html structure.

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,25 +1,19 @@
 
-
 export default function Cards({ results }) {
 
-    // const [ characters, setCharacters ] = setState(results)
-    
-    // if (characters.length > 0) {
-
-        const cards = results.map(char => {
-            return (
-                <div className="character--card">
-                    <img src={char.image} alt={`${char.name}`} />
-                    <div className="card--stats">
-                        <h3>{char.name}</h3>
-                        <p><b>Species:</b> {char.species}</p>
-                        <p><b>Gender:</b> {char.gender}</p>
-                        <p><b>Status:</b> {char.status}</p>
-                    </div>
+    const cards = results.map(char => {
+        return (
+            <div className="character--card">
+                <img src={char.image} alt={`${char.name}`} />
+                <h3>{char.name}</h3>
+                <div className="card--stats">
+                    <p><b>Species:</b> {char.species}</p>
+                    <p><b>Gender:</b> {char.gender}</p>
+                    <p><b>Status:</b> {char.status}</p>
                 </div>
+            </div>
         )
-        })
-    // }
+    })
 
     return (
         <section className="cards">
